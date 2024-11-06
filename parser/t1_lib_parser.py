75f9cfe00d3cd1c74c1a93b53901d3a64b2a4c3e
atemcozz <atemcozzartem@gmail.com>
2024-11-06 21:51:25 +0300

    Report changes

--- parser/t1_lib_parser.py
+++ parser/t1_lib_parser.py
@@ -7,7 +7,7 @@ def yaml_to_json(string):
 
 
 if __name__ == "__main__":
-    with open("../resources/schedule.yml", "r", encoding="UTF-8") as fin, open("out.json", "w",
+    with open("../resources/schedule.yml", "r", encoding="UTF-8") as fin,open("out.json", "w",
                                                                                encoding="UTF-8") as fout:
         content = fin.read()
         result = yaml_to_json(content)
